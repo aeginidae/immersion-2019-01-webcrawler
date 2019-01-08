cd8b6dec82cf254a29a0d2426b86859ff57eba59
aeginidae <aeginidae@gmail.com>
2019-01-08 11:00:41 -0600

    maybe made it work?

--- webcrawler.js
+++ webcrawler.js
@@ -2,14 +2,16 @@ const webCrawler = (url, max = 10) => {
   // url is the starting point of the crawl
   // max is the maximum amount of urls you want to crawl through
   // default is 10 for performance purposes
-  const visited = [];
+  const visited = [],
+  foundURLs = [];
   let crawlCount = 0;
   const crawl = (crawled = url) => {
-    if (visited.includes(crawled) || crawlCount > max) return;
+    if (visited.includes(crawled)) return;
+    else if (crawlCount >= max) return foundURLs;
     else {
       crawlCount++;
       visited.push(crawled);
-      console.log(crawlCount, visited);
+      // console.log(crawlCount, visited);
       const request = new XMLHttpRequest();
       request.open('GET', crawled);
       request.responseType = 'document';
@@ -20,7 +22,7 @@ const webCrawler = (url, max = 10) => {
             // console.log(linkTag.href.replace(/(#.*$)|(\$.*$)/, '') === body.baseURI);
             if (linkTag.href.replace(/(#.*$)|(\$.*$)/, '') === body.baseURI) return;
             else {
-              console.log(linkTag.href);
+              foundURLs.push(linkTag.href);
               crawl(linkTag.href);
             }
           });
